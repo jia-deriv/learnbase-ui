e5308d8334eb5d7b810269baf6207177af81aed5
jia-wei-00 <36665844+jia-wei-00@users.noreply.github.com>
2024-01-14 05:19:24 +0000

    Deploying to gh-pages from  @ 1df5047def3d055770f8cdaed5112a54c418ec50 🚀

---
+++ assets/preview-0IbiCLeu.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-Lx9nGHL0.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-ueLr-Iy4.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-ueLr-Iy4.js","./iframe-Lx9nGHL0.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-0wh8ubQX.js","./_getPrototype-MXemwaK5.js","./_baseIsEqual-CFr8iVHd.js","./uniq-DjaUhnlG.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
