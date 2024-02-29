90e16f6d935e916361ec72f3b1b3e61469dccf4b
jia-deriv <jiawei@regentmarkets.com>
2024-03-01 01:31:57 +0800

    fix: button group vertical border issues

--- libs/button/src/lib/button-group/index.tsx
+++ libs/button/src/lib/button-group/index.tsx
@@ -20,14 +20,57 @@ const buttonGroupClass = cva(
         contained: '[&>button]:border-white',
         outlined: '',
       },
-      rounded: {
-        none: '',
-        sm: '[&>button:first-child]:rounded-l [&>button:last-child]:rounded-r',
-        md: '[&>button:first-child]:rounded-l-md [&>button:last-child]:rounded-r-md',
-        lg: '[&>button:first-child]:rounded-l-lg [&>button:last-child]:rounded-r-lg',
-        full: '[&>button:first-child]:rounded-l-full [&>button:last-child]:rounded-r-full',
-      },
     },
+    compoundVariants: [
+      {
+        orientation: 'vertical',
+        rounded: 'sm',
+        class:
+          '[&>button:first-child]:rounded-t [&>button:last-child]:rounded-b',
+      },
+      {
+        orientation: 'vertical',
+        rounded: 'md',
+        class:
+          '[&>button:first-child]:rounded-t-md [&>button:last-child]:rounded-b-md',
+      },
+      {
+        orientation: 'vertical',
+        rounded: 'lg',
+        class:
+          '[&>button:first-child]:rounded-t-lg [&>button:last-child]:rounded-b-lg',
+      },
+      {
+        orientation: 'vertical',
+        rounded: 'full',
+        class:
+          '[&>button:first-child]:rounded-t-full [&>button:last-child]:rounded-b-full',
+      },
+      {
+        orientation: 'horizontal',
+        rounded: 'sm',
+        class:
+          '[&>button:first-child]:rounded-l [&>button:last-child]:rounded-r',
+      },
+      {
+        orientation: 'horizontal',
+        rounded: 'md',
+        class:
+          '[&>button:first-child]:rounded-l-md [&>button:last-child]:rounded-r-md',
+      },
+      {
+        orientation: 'horizontal',
+        rounded: 'lg',
+        class:
+          '[&>button:first-child]:rounded-l-lg [&>button:last-child]:rounded-r-lg',
+      },
+      {
+        orientation: 'horizontal',
+        rounded: 'full',
+        class:
+          '[&>button:first-child]:rounded-l-full [&>button:last-child]:rounded-r-full',
+      },
+    ],
     defaultVariants: {
       orientation: 'horizontal',
     },
